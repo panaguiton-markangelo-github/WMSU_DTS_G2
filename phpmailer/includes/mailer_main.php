2d9d8233320817752b40030c31b05a0f6d0181ec
kookie1721 <drenegades19@gmail.com>
2022-03-26 22:52:07 +0800

    test

--- phpmailer/includes/mailer_main.php
+++ phpmailer/includes/mailer_main.php
@@ -10,14 +10,6 @@ use PHPMailer\PHPMailer\Exception;
 
 $mail = new PHPMailer();
 
-$mail->SMTPOptions = array(
-    'ssl' => array(
-    'verify_peer' => false,
-    'verify_peer_name' => false,
-    'allow_self_signed' => true
-    )
-);
-
 $mail->isSMTP();
 
 $mail->Host = "smtp.gmail.com";
