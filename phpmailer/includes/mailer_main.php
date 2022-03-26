d8a93310b067d0f6f4c3c6955ec9479bae3e043c
kookie1721 <drenegades19@gmail.com>
2022-03-26 22:36:29 +0800

    test

--- phpmailer/includes/mailer_main.php
+++ phpmailer/includes/mailer_main.php
@@ -10,12 +10,23 @@ use PHPMailer\PHPMailer\Exception;
 
 $mail = new PHPMailer();
 
+$mail->SMTPOptions = array(
+    'ssl' => array(
+    'verify_peer' => false,
+    'verify_peer_name' => false,
+    'allow_self_signed' => true
+    )
+);
+
 $mail->isSMTP();
 
-$mail->Host = "localhost";
-$mail->SMTPAuth = false;
-$mail->SMTPAuthTLS = false;
-$mail->Port = 25;
+$mail->Host = "smtp.gmail.com";
+
+$mail->SMTPAuth = true;
+
+$mail->SMTPSecure = "tls";
+
+$mail->Port = 587;
 
 $mail->Username = "wmsudts.noreply@gmail.com";
 
