fc2c4e7fed71b3f318fd851937d570404c2fa79b
kookie1721 <drenegades19@gmail.com>
2022-04-12 13:54:29 +0800

    updated version

--- index.php
+++ index.php
@@ -57,12 +57,7 @@
 					</div>
 					
 					<div class="flex-sb-m w-full p-b-48">
-						<div class="contact100-form-checkbox">
-							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-							<label class="label-checkbox100" for="ckb1">
-								Remember me
-							</label>
-						</div>
+						
 
 						<div>
 							<a href="forgot_pass/forgot-pass.php" class="txt3">
