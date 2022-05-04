948c72e5119290c8f1e8b3753cc46edd6ab4cb1b
kookie1721 <drenegades19@gmail.com>
2022-05-04 15:33:50 +0800

    updates

--- clerk/view_documentC.php
+++ clerk/view_documentC.php
@@ -14,6 +14,7 @@ if(!isset($_SESSION["c_username"])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/footer.css">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
 </head>
 <body style="background-color:#fff;">
     <nav class="navbar navbar-expand-sm navbar-dark border-bottom border-dark" style="background-color:#8e0413;">
