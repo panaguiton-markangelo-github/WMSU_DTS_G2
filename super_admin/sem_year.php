5c730e4286cf04c1e24e20289de2d858d8ec06e4
kookie1721 <drenegades19@gmail.com>
2022-05-17 00:01:51 +0800

    updates

--- super_admin/sem_year.php
+++ super_admin/sem_year.php
@@ -31,6 +31,7 @@ catch(PDOException $e) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<base href="/" />
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
