e1d9a25bbe7d950247511608f4376f212f176ead
kookie1721 <drenegades19@gmail.com>
2022-05-17 00:31:51 +0800

    updates

--- super_admin/HomePageSA.php
+++ super_admin/HomePageSA.php
@@ -21,7 +21,7 @@ catch(PDOException $e) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <base href="/" />
+    <base target="_self"/>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-sacle=1">
