aabea575c25fab60c837be88a3f48b0517991613
kookie1721 <drenegades19@gmail.com>
2022-05-17 02:43:55 +0800

    updates

--- validation/process_login.php
+++ validation/process_login.php
@@ -33,18 +33,21 @@
             if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
                 $_SESSION['invalid_email'] = "Please enter a valid email!!";
                 header('location: index.php?invalid=email');
+                die();
             }
-    
-            else {
+            else if(filter_var($username, FILTER_VALIDATE_EMAIL)){
                 $sql = "SELECT * FROM users WHERE username = '".$username."' AND activated = 'yes' ;";
                 $result = mysqli_query($data, $sql);
                 $row = mysqli_fetch_array($result);
                 if(empty($row)){
                     $_SESSION['invalid_match'] = "The user account was deactivated, please ask the super admin to activate your account or your office admin if you're a clerk.";
                     header('Location: index.php?invalid=deactivated');
-                    exit();
+                    die();
                 } 
-
+            }
+    
+            else {
+               
                 $sql = "SELECT * FROM users WHERE username = '".$username."' ;";
                 $result = mysqli_query($data, $sql);
                 $row = mysqli_fetch_array($result);
