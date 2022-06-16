8a271825dd72f6539d4c366391cae12f3c1f5fb1
kookie1721 <drenegades19@gmail.com>
2022-06-16 21:02:44 +0800

    updates

--- include/forgot-pass-reset.php
+++ include/forgot-pass-reset.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 session_start();
 
-require '../phpmailer/includes/mailer_main.php';
+require ($_SERVER['DOCUMENT_ROOT'] . '/phpmailer/includes/mailer_main.php');
 
 
 if (isset($_POST['reset-submit'])) {
