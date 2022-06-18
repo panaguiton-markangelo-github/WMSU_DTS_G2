1a0fe8879ee211b7686527916ccf1eab12ee12e5
kookie1721 <drenegades19@gmail.com>
2022-06-18 20:38:50 +0800

    updates

--- validation/logout.php
+++ validation/logout.php
@@ -4,6 +4,7 @@ if(isset($_POST['logout'])) {
     session_unset();
     session_abort();
     session_destroy();
+    header("Cache-Control", "no-cache, no-store, must-revalidate");
     header("location: ../index.php?new=login");
     exit();
 }
