87423d6694e7f698fc5d6c933258523f7cb6a582
kookie1721 <drenegades19@gmail.com>
2022-06-18 20:58:57 +0800

    updates

--- super_admin/HomePageSA.php
+++ super_admin/HomePageSA.php
@@ -1,4 +1,9 @@
 <?php
+header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
+
 session_start();
 if(!isset($_SESSION["sa_username"])) {
   header("location: ../index.php");
