001b6549998900058ca987f962f2a8ba1dc90472
kookie1721 <drenegades19@gmail.com>
2022-06-18 21:17:58 +0800

    updates

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
-if($_GET["new"]){
 	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 	header("Cache-Control: post-check=0, pre-check=0", false);
 	header("Pragma: no-cache");
 	header('Content-Type: text/html'); 
-}
 
 include('validation/process_login.php');
 ?>
