a24389724d747fdf7ccfd859a649f944ebe188b8
kookie1721 <drenegades19@gmail.com>
2022-06-18 20:44:56 +0800

    updates

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php include('validation/process_login.php');?>
+<?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header('Content-Type: text/html'); 
+include('validation/process_login.php');
+?>
 
 <!DOCTYPE html>
 <html lang="en" >
--- validation/logout.php
+++ validation/logout.php
@@ -2,9 +2,7 @@
 session_start();
 if(isset($_POST['logout'])) {
     session_unset();
-    session_abort();
     session_destroy();
-    header("Cache-Control", "no-cache, no-store, must-revalidate");
     header("location: ../index.php?new=login");
     exit();
 }
