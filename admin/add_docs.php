b5b8e87b1769e98b6369f62e1da465fe100d5b4a
kookie1721 <drenegades19@gmail.com>
2022-06-23 20:20:39 +0800

    updates

--- admin/add_docs.php
+++ admin/add_docs.php
@@ -43,8 +43,17 @@ catch(PDOException $e) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/footer.css">
+    <link rel="stylesheet" href="../assets/css/checkbox.css">
 </head>
 <body style="background-color:#fff;">
+    <script>
+      function toggle(source) {
+        checkboxes = document.getElementsByClassName('officeName');
+        for(var i=0, n=checkboxes.length;i<n;i++) {
+          checkboxes[i].checked = source.checked;
+        }
+      }
+    </script>
     <nav class="navbar navbar-expand-sm navbar-dark border-bottom border-dark" style="background-color:#8e0413;">
         <div class="container-fluid">
           <a class="navbar-brand fw-bold">WMSU|DTS</a>
@@ -184,6 +193,35 @@ catch(PDOException $e) {
               </div>
               <p class="text-center text-muted fw-bold"> Max Length(optional): <mark>100</mark>characters.</p>
             </div>      
+          </div>
+
+          <div class="row">
+            <div class="col">
+              <p class="text-center text-muted fw-bold">Please select the recipient/s. Note: You can select more than one.</p>
+              <div class="boxes">
+                  <input name="select-all" onClick="toggle(this)" type="checkbox" id="select-all" value="select-all">
+                  <label for="select-all">All</label>
+                <?php
+                      $database = new Connection();
+                      $db = $database->open();
+                      try{	
+                          $sql = 'SELECT * FROM office ORDER BY officeName ASC;'; 
+                          foreach ($db->query($sql) as $row1) {
+                    ?>
+                      <input class="officeName" name="officeName[]" type="checkbox" id="<?php echo $row1['officeName'];?>" value="<?php echo $row1['officeName'];?>">
+                      <label for="<?php echo $row1['officeName'];?>"><?php echo $row1['description'];?></label>
+                      <?php
+                      }
+
+                    }
+                    catch(PDOException $e){
+                        echo "There is some problem in connection: " . $e->getMessage();
+                    }
+                    //close connection
+                    $database->close();
+                  ?>
+              </div>
+            </div>
           </div>
 
           <input name="user_id" type="number" value="<?php echo $_SESSION['userID'];?>" hidden>
