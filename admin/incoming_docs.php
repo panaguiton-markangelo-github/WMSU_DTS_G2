88d594dd35ff99100e993d577589e5688d5b5350
kookie1721 <drenegades19@gmail.com>
2022-06-25 21:49:04 +0800

    updates

--- admin/incoming_docs.php
+++ admin/incoming_docs.php
@@ -103,7 +103,10 @@ if(!isset($_SESSION["a_username"])) {
                 Incoming Documents
             </h2>
 
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="glyphicon glyphicon-bell" style="font-size:18px;"></span></a>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="glyphicon glyphicon-bell" style="font-size:18px;"></span></a>
+                <ul class="dropdown-menu"></ul>
+            </li>
 
             <div class="user-wrapper">
                 <div class="profile" onclick="menuToggle();">
