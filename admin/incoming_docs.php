6a4b7d353b3f7dd7ef3656b61e8f9eb6cf2328d6
kookie1721 <drenegades19@gmail.com>
2022-06-25 21:50:28 +0800

    updates

--- admin/incoming_docs.php
+++ admin/incoming_docs.php
@@ -104,7 +104,7 @@ if(!isset($_SESSION["a_username"])) {
             </h2>
 
             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="glyphicon glyphicon-bell" style="font-size:18px;"></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="las la-bell" style="font-size:18px;"></span></a>
                 <ul class="dropdown-menu"></ul>
             </li>
 
