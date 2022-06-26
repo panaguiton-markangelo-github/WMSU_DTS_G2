0c3223cc52f366ec4098d1e46879f5045f098ad8
kookie1721 <drenegades19@gmail.com>
2022-06-26 21:09:34 +0800

    updates

--- clerk/add_docs.php
+++ clerk/add_docs.php
@@ -44,6 +44,7 @@ catch(PDOException $e) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/footer.css">
+    <link rel="stylesheet" href="../assets/css/checkbox.css">
 </head>
 <body style="background-color:#fff;">
     <nav class="navbar navbar-expand-sm navbar-dark border-bottom border-dark" style="background-color:#8e0413;">
