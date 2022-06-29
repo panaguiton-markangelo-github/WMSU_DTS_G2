b3ef3be50f94b3eff9beaa94aacd3d1f18d17d90
kookie1721 <drenegades19@gmail.com>
2022-06-29 22:06:54 +0800

    updates

--- admin_funcs/add_doc.php
+++ admin_funcs/add_doc.php
@@ -15,20 +15,7 @@ error_reporting(E_ALL);
 		$reason = $_POST['reason'];
 
 		
-		foreach($_POST['officeName'] as $office){
-			global $users;
-			$users = array();
-			$sql1 = "SELECT username FROM users WHERE officeName = '".$office."';";
-			$result1 = mysqli_query($data, $sql1);
-			$row = mysqli_fetch_assoc($result1);
-			$users[] = $row['username'];
-
-			
-		}
-
-		$no = 0;
-		$no1 = 0;
-
+		
 	
 
 
