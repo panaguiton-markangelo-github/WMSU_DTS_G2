0d6d9b9703b724d28909935327179f1df88798d8
kookie1721 <drenegades19@gmail.com>
2022-06-30 23:47:59 +0800

    updates

--- admin_funcs/receive.php
+++ admin_funcs/receive.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+#ini_set('display_errors', 1);
+#ini_set('display_startup_errors', 1);
+#error_reporting(E_ALL);
 //include our connection
 require '../phpmailer/includes/mailer_main.php';
 include_once('../include/database.php');
